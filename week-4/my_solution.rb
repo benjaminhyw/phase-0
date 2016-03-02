fbc46448ac64ff5517a7f460f5b2295b9c22a54e
Ben Flores <benjaminhyw@gmail.com>
2016-03-01 17:07:00 -0800

    created gps 2 files

---
+++ week-4/my_solution.rb
@@ -0,0 +1,13 @@
+# Define an Empty Method
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+
+def do_something
+	puts fav_color
+	puts fav_shape
+	puts fav_fruit
+end
---
+++ week-4/variables-methods.rb
@@ -0,0 +1,12 @@
+puts "What is your first name?"
+first_name=gets.chomp
+puts "How about a middle name?"
+middle_name=gets.chomp
+puts "Last name?"
+last_name=gets.chomp
+puts "Well hello, " + first_name + " " + middle_name + " " + last_name + "!!"
+
+
+puts "What is your favorite number?"
+fav_num=gets.chomp
+puts "So your favorite number is " + fav_num + "?  Why not " + fav_num.next + " instead?"
