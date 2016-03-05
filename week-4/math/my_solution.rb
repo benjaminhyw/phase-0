67b3128037b8765dd662de50f83221729b59230f
Ben Flores <benjaminhyw@gmail.com>
2016-03-04 21:28:48 -0800

    Fixed my method, removed puts replaced with return

--- week-4/math/my_solution.rb
+++ week-4/math/my_solution.rb
@@ -7,20 +7,22 @@
 
 def add(num_1, num_2)
  #your code here
- puts num_1+num_2
+ return num_1+num_2
 end
+
+
 
 def subtract(num_1, num_2)
  #your code here
- puts num_1-num_2
+ return num_1-num_2
 end
 
 def multiply(num_1, num_2)
  #your code here
- puts num_1 * num_2
+ return num_1 * num_2
 end
 
 def divide(num_1, num_2)
  #your code here
- puts num1.to_f/num2.to_f
+ return num_1.to_f/num_2.to_f
 end
