632f5714999440d043d9d3fbc0ceace8101190cb
Ben Flores <benjaminhyw@gmail.com>
2016-03-06 18:58:56 -0800

    added pseudocode to solution for factorial_spec.rb

--- week-4/factorial/my_solution.rb
+++ week-4/factorial/my_solution.rb
@@ -1,5 +1,15 @@
 # Factorial
 
+
+#First we define the factorial of 0 == 1
+#Assign variable num = number-1, and result = number (as in new number), do this to count down
+#WHILE variable num is greater than or equal to 1, 
+	#result multiplies all of the numbers within num,
+	#then we subtract 1 from num
+	#end
+#print result
+#end
+
 # I worked on this challenge [by myself, with: ].
 
 
