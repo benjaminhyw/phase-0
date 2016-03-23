f2b5660f22f1196ab4626665aa2b19aff7b4334a
Ben Flores <benjaminhyw@gmail.com>
2016-03-23 16:51:19 -0700

    copied tests.js from provided set, created group_project_solution.js file and added User Stories (assignment for person 1)

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,20 @@
+/*
+User Stories written by Person 1 (Ben Flores)
+
+
+---SUM---
+1. As a user, I want to have a function called sum.
+2. As a user, sum should give me the sum of ALL of the elements in an array that has an odd number of elements.
+3. As a user, sum should give me the sum of ALL of the elements in an array that has an even number of elements.
+
+---MEAN---
+4. As a user, I want to have a function called mean.
+5. As a user, mean should give me the average of ALL of the elements in an array that has an odd number of elements.  I need an exact amount, not a rounded integer.
+6. As a user, mean should give me the average of ALL of the elements in an array that has an even number of elements.  I need an exact amount, not a rounded integer.
+
+---MEDIAN---
+7. As a user, I want to have a function called median.
+8. As a user, median should give me the median value of ALL of the elements in an array that has an odd number of elements.
+9. As a user, median should give me the median value of ALL of the elements in an array thta has an even number of elements.  I need an exact amount, not a rounded integer.
+
+*/
